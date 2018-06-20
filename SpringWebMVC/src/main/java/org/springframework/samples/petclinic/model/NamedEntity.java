932be55a376a6c3aafb5db12d8bdcfa195f9982c
Rahulkumarpatel <rahulkumar.patel1984@gmail.com>
2018-06-21 01:08:33 +0530

    fdfsdfsdfsdfsdfsdf

--- SpringWebMVC/src/main/java/org/springframework/samples/petclinic/model/NamedEntity.java
+++ SpringWebMVC/src/main/java/org/springframework/samples/petclinic/model/NamedEntity.java
@@ -22,7 +22,7 @@ import javax.persistence.MappedSuperclass;
 /**
  * Simple JavaBean domain object adds a name property to <code>BaseEntity</code>. Used as a base class for objects
  * needing these properties.
- *  dsfdsf  fsdfsdf
+ *  dsfdsf  fsdfsdf dfsdafsdf
  * @author Ken Krebs
  * @author Juergen Hoeller
  */
