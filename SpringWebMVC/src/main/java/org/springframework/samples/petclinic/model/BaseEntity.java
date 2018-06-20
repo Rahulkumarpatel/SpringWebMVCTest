c4a48cf596d3fd520da0d61f698573d9c7ddc289
Rahulkumarpatel <rahulkumar.patel1984@gmail.com>
2018-06-21 00:44:25 +0530

    Committing new changes in gitHub Repo

--- SpringWebMVC/src/main/java/org/springframework/samples/petclinic/model/BaseEntity.java
+++ SpringWebMVC/src/main/java/org/springframework/samples/petclinic/model/BaseEntity.java
@@ -25,7 +25,7 @@ import javax.persistence.MappedSuperclass;
 /** 
  * Simple JavaBean domain object with an id property. Used as a base class for objects
  * needing this property.  
- *  
+ *   dsfdsfdsf 
  * @author Ken Krebs
  * @author Juergen Hoeller
  */
--- SpringWebMVC/src/main/java/org/springframework/samples/petclinic/model/package-info.java
+++ SpringWebMVC/src/main/java/org/springframework/samples/petclinic/model/package-info.java
@@ -1,5 +1,5 @@
 /**
- * The classes in this package represent utilities used by the domain.
+ * The classes in this package represent utilities used by the domain.fdgfd 
  */
 package org.springframework.samples.petclinic.model;
 
